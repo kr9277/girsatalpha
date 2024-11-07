28966b237f3ecfd333f13b7c498c59408eb0fb32
karin <kr9277@bs.amalnet.k12.il>
2024-11-07 17:25:10 +0200

    tafrit&time

--- app/src/main/java/com/example/girsatalpha/MainActivity.java
+++ app/src/main/java/com/example/girsatalpha/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.girsatalpha;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
+import android.view.Menu;
+import android.view.MenuItem;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,5 +31,33 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.tafrit, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    public boolean onOptionsItemSelected(@Nullable MenuItem item){
+        String st = item.getTitle().toString();
+        if(st.equals("Enter Data Activity")){
+            Intent intent = new Intent(this, EnterDataActivity.class);
+            startActivity(intent);
+        }
+        else if(st.equals("GPS Activity")){
+            Intent intent = new Intent(this, GPSActivity.class);
+            startActivity(intent);
+        }
+        else if(st.equals("Show Data")){
+            Intent intent = new Intent(this, ShowData.class);
+            startActivity(intent);
+        }
+        else if(st.equals("Sound Activity")){
+            Intent intent = new Intent(this, SoundActivity.class);
+            startActivity(intent);
+        }
+        else if(st.equals("Time Picker Activity")){
+            Intent intent = new Intent(this, TimePickerActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/girsatalpha/TimePickerActivity.java
+++ app/src/main/java/com/example/girsatalpha/TimePickerActivity.java
@@ -1,14 +1,25 @@
 package com.example.girsatalpha;
 
+import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TimePicker;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class TimePickerActivity extends AppCompatActivity {
     Button btnOpenTime;
@@ -26,5 +37,51 @@ public class TimePickerActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void setTime(View view) {
+        Calendar calendar = Calendar.getInstance();
+        int hours = calendar.get(Calendar.HOUR_OF_DAY);
+        int mins = calendar.get(Calendar.MINUTE);
+        TimePickerDialog timePickerDialog = new TimePickerDialog(TimePickerActivity.this, com.google.android.material.R.style.Theme_AppCompat_Dialog, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                Calendar c = Calendar.getInstance();
+                c.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                c.set(Calendar.MINUTE, minute);
+                c.setTimeZone(TimeZone.getDefault());
+                SimpleDateFormat format = new SimpleDateFormat("k:mm a");
+                String time = format.format(c.getTime());
+            }
+        }, hours, mins, false);
+        timePickerDialog.show();
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.tafrit, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    public boolean onOptionsItemSelected(@Nullable MenuItem item){
+        String st = item.getTitle().toString();
+        if(st.equals("Enter Data Activity")){
+            Intent intent = new Intent(this, EnterDataActivity.class);
+            startActivity(intent);
+        }
+        else if(st.equals("GPS Activity")){
+            Intent intent = new Intent(this, GPSActivity.class);
+            startActivity(intent);
+        }
+        else if(st.equals("Show Data")){
+            Intent intent = new Intent(this, ShowData.class);
+            startActivity(intent);
+        }
+        else if(st.equals("Sound Activity")){
+            Intent intent = new Intent(this, SoundActivity.class);
+            startActivity(intent);
+        }
+        else if(st.equals("Main Activity")){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
